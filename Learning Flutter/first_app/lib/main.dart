0ee9c2a60e63debb48a0091fd333161bae134242
Michael Su <mdart167@gmail.com>
2024-12-31 16:20:55 -0500

    Added background and changed text style

--- Learning Flutter/first_app/lib/main.dart
+++ Learning Flutter/first_app/lib/main.dart
@@ -1,37 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+  //define the main function
+  runApp(
+    MaterialApp(
       home: Scaffold(
-        appBar: AppBar(
-          title: const Text('First App'),
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              Text(
-                'Hello World!',
-              ),
-              Text(
-                'It\'s time to learn Flutter!',
-              ),
-            ],
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Colors.tealAccent, Colors.purple],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              )
+          ),
+          child: const Center(
+            child: Text('hello',
+            style:TextStyle(
+              fontSize: 40,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+            ),
+            
           ),
         ),
       ),
-    );
-  }
+    ),
+  ); //function to run the app and show the UI on screen
 }
